419c2db4e9d992d944895a93f569c68e6f86ca4f
trulstveoy <t@ruls.s>
2015-10-28 19:10:51 +0100

    more cleanup

--- src/mainPage.ts
+++ src/mainPage.ts
@@ -1,6 +1,32 @@
-import {EventData} from 'data/observable';
 import {Page} from 'ui/page';
-import MainPageModel from './mainPageModel';
+import {EventData, Observable} from 'data/observable';
+import {ObservableArray} from 'data/observable-array';
+import * as http from 'http';
+
+class MainPageModel extends Observable {
+    
+    private items = new ObservableArray<string>();
+    
+    constructor() {
+        super();   
+    }
+    
+    onSearch() {                
+        const searchText = this.get('searchText');        
+        const url = 'http://celery.azurewebsites.net/api/food/?query=' + searchText;
+            
+        http.getJSON(url).then((items:any) => {
+            this.items.push(items);           
+        }, e => {
+           this.set('output', e);
+        });
+    }
+    
+    clearSearch(){
+        this.items.splice(0);
+        this.set('output', 'clear');
+    }    
+}
 
 export function pageLoaded(args: EventData) {    
     var page = <Page>args.object;
